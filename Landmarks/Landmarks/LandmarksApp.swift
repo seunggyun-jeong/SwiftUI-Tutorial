1937895ef4951056aa193cd0b956914678319c1e
seunggyun-jeong <jskyun98@gmail.com>
2022-05-17 20:28:35 +0900

    Swift Tutorial Day 1

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 정승균 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
