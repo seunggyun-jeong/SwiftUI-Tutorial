5c030857510a6fd681334dc35a162c045f953682
seunggyun-jeong <jskyun98@gmail.com>
2022-05-19 23:45:06 +0900

    Swift Tutorial Day 3

---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -0,0 +1,40 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 정승균 on 2022/05/18.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landMark: Landmark
+    
+    var body: some View {
+        HStack {
+            landMark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landMark.name)
+            
+            Spacer()
+            
+            if landMark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        Group{
+            LandmarkRow(landMark: landmarks[0])
+            LandmarkRow(landMark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
