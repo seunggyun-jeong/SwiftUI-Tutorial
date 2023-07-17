c3d93c9d96db1d15100c04c7eeb513e321f34495
seunggyun-jeong <jskyun98@gmail.com>
2023-07-17 15:46:47 +0900

    [학습] Core Data Relationship

---
+++ CoreDataBootcamp/CoreDataBootcamp/CoreDataBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootcampApp.swift
+//  CoreDataBootcamp
+//
+//  Created by 정승균 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoreDataRelationshipsBootcamp()
+        }
+    }
+}
