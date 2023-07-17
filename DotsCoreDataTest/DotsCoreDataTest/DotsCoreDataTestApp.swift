7d5dc4ccce0b7212b3edf35e27ccff01944fed7e
seunggyun-jeong <jskyun98@gmail.com>
2023-07-17 15:59:40 +0900

    [테스트] MC3 프로젝트를 위한 Core Data Test

---
+++ DotsCoreDataTest/DotsCoreDataTest/DotsCoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotsCoreDataTestApp.swift
+//  DotsCoreDataTest
+//
+//  Created by 정승균 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct DotsCoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
